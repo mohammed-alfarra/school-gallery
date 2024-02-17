2126931b321345f2e474eeaf435969ec8957fe2d
Mohammed Alfarra <mbhis19@gmail.com>
2024-02-17 15:50:36 +0300

    init project with auth && github actions

---
+++ routes/dashboard.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\API\Dashboard\AuthController;
+use Illuminate\Support\Facades\Route;
+
+/*===================================
+=            admin login           =
+===================================*/
+Route::post('login', [AuthController::class, 'login'])->name('login');
+Route::post('logout', [AuthController::class, 'logout']);
+Route::post('me', [AuthController::class, 'me']);
+/*=====  End of admin login  ======*/
